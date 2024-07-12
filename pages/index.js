9df39112863271b9e2eb6b05a78474a807367a88
Ricardo Nakano <ricardo.nakano@outlook.com>
2024-07-12 21:55:44 +0000

    poema

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Te amo mihnha linda! Se você também me ama, dê uma risadinha!</h1>
+    return <h1>Linda, meu amor, te amo sem fim,</h1><br>
+    <h1>Se você me ama, jante hoje comigo assim.</h1><br>
+    <h1>Com humor e carinho, nosso amor é especial,</h1><br>
+    <h1>Vamos jantar juntos, num momento sensacional!</h1><br>
 }
 
 export default Home;
