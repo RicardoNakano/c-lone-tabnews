cd1ae8d6d9b5bfee98c0895f325d059920ec769d
Ricardo Nakano <ricardo.nakano@outlook.com>
2024-07-12 22:01:08 +0000

    novo poema

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 function Home() {
-    return <h1>Linda, meu amor, te amo sem fim,</h1><br>
-    <h1>Se você me ama, jante hoje comigo assim.</h1><br>
-    <h1>Com humor e carinho, nosso amor é especial,</h1><br>
-    <h1>Vamos jantar juntos, num momento sensacional!</h1><br>
+    return <h1>Linda, meu amor, te amo sem fim,<br>Se você me ama, jante hoje comigo assim.</br></h1>  
 }
 
 export default Home;
