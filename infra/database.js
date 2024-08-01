86c19618b6c3ff806fba190e8800e590db55d11f
Ricardo Nakano <ricardo.nakano@outlook.com>
2024-08-01 22:21:04 +0000

    tira o console.log das credenciais

--- infra/database.js
+++ infra/database.js
@@ -9,15 +9,6 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("Credenciais do Postgres", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    //password: process.env.POSTGRES_PASSWORD,
-    //is_enviroment_not_dev:
-    //  process.env.NODE_ENV === "development" ? false : true,
-  });
 
   try {
     await client.connect();
